47964d0de81dc10201b23c72b84a9619ab525e32
mnjoki21 <mitchellenjoki7@gmail.com>
2022-09-27 07:38:29 +0300

    using post

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,9 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create
+    bird = Bird.create(name: params[:name], species: paramns[:species])
+    render json: bird, status: :created
+  end
+
 end
